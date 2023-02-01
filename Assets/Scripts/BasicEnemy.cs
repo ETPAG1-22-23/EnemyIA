30751dc409948f8f93389e052c93874c69624bbd
RBasic <rcham.pro@gmail.com>
2023-02-01 15:03:11 +0100

    Test Basic IA

---
+++ Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator animController;
+    float horizontal_value = 1;
+    Vector2 ref_velocity = Vector2.zero;
+
+    float jumpForce = 12f;
+
+    [SerializeField] float moveSpeed_horizontal = 400.0f;
+    [SerializeField] bool is_jumping = false;
+    [SerializeField] bool can_jump = false;
+    [Range(0, 1)][SerializeField] float smooth_time = 0.5f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(horizontal_value > 0) sr.flipX = false;
+        else if (horizontal_value < 0) sr.flipX = true;
+    }
+    void FixedUpdate()
+    {
+        Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.fixedDeltaTime, rb.velocity.y);
+        rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, 0.05f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.tag != "Player")
+        {
+            horizontal_value *= -1;
+        }
+    }
+}
