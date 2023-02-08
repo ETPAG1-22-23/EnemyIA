915df3b3082fc5b990be3ac5a03a9eda3097ef4e
Sevirakath <tomstef34@gmail.com>
2023-02-08 13:00:09 +0100

    AI Saut Test (Ne marche pas encore)

---
+++ Assets/IA Mouvement avec saut.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyChaseBird : MonoBehaviour
+{
+
+    public Transform target;
+    public float speed = 5f;
+    private void Update()
+  {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        if (true)
+        {
+            transform.position = new Vector3(transform.position.x, -3.47f, transform.position.z);
+
+        }
+        private Rigidbody rb;
+    public bool onG;
+    float repeats;
+    public float amount;
+    public float rotateSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        onG = true;
+        repeats = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (repeats < amount)
+        {
+            if (onG)
+            {
+                rb.velocity = new Vector3(0, 1, 1);
+                onG = false;
+                repeats = +1;
+            }
+        }
+        if (repeats == amount)
+        {
+            transform.Rotate(0, rotateSpeed, 0);
+            repeats = 0;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.CompareTag("ground"))
+        {
+            onG = true;
+        }
+    }
+}
